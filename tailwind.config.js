d380688156a2ef10ff653ba04c76ebe782513409
vorona1707 <vitosin1990@gmail.com>
2024-02-10 15:30:08 +0300

    other

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage:{
+      'fruits-bg': 'url(/image/banner.jpg)'
+     }
+    },
+  },
+  plugins: [],
+}
+
