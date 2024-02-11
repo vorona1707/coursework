7c399c8d200c6bc243f9e0f5dd6a053057a859b4
vorona1707 <vitosin1990@gmail.com>
2024-02-11 23:46:47 +0300

    new commit

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ type User struct {
 
 func main() {
 
-	mux := mux.NewRouter()
+	router := mux.NewRouter()
 
 	db, err := gorm.Open(sqlite.Open("data.db"), &gorm.Config{})
 	if err != nil {
@@ -40,7 +40,7 @@ func main() {
 		return
 	}
 
-	mux.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
 		tmpl, err := template.ParseFiles("static/reg.tmpl")
 		if err != nil {
 			fmt.Printf("Error parsing template: %v\n", err)
@@ -62,9 +62,9 @@ func main() {
 		}
 	})
 
-	mux.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
-	mux.HandleFunc("/reg", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("/reg", func(w http.ResponseWriter, r *http.Request) {
 
 		err := r.ParseForm()
 		if err != nil {
@@ -87,6 +87,11 @@ func main() {
 
 		if surname == "" {
 			fmt.Printf("ошибка фамилии \n")
+			return
+		}
+
+		if password == "" {
+			fmt.Printf("ошибка пароля \n")
 			return
 		}
 
@@ -115,11 +120,12 @@ func main() {
 			fmt.Printf("Ошибка \n")
 			return
 		}
+
 		http.Redirect(w, r, "/register", http.StatusFound)
 
 	})
 
-	mux.HandleFunc("/deleteform", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("/deleteform", func(w http.ResponseWriter, r *http.Request) {
 
 		tmpl, err := template.ParseFiles("static/deldb.tmpl")
 		if err != nil {
@@ -135,6 +141,8 @@ func main() {
 			return
 		}
 
+		fmt.Printf("%+v \n", users)
+
 		err = tmpl.Execute(w, users)
 		if err != nil {
 			fmt.Printf("Error executing template: %v\n", err)
@@ -142,11 +150,45 @@ func main() {
 		}
 	})
 
-	mux.HandleFunc("/delid", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("/edit", func(w http.ResponseWriter, r *http.Request) {
 
-		err := r.ParseForm()
+		tmpl, err := template.ParseFiles("static/edit_users.tmpl")
 		if err != nil {
-			fmt.Printf("Ошибка формы: %v\n", err)
+			fmt.Printf("Error parsing template: %v\n", err)
+			return
+		}
+
+		var users []*User
+		err = db.Find(&users).Error
+		if err != nil {
+			fmt.Printf("Говно вопрос: %v\n", err)
+			return
+		}
+
+		fmt.Printf("%+v \n", users)
+
+		err = tmpl.Execute(w, users)
+		if err != nil {
+			fmt.Printf("Error executing template: %v\n", err)
+			return
+		}
+
+	})
+
+	router.HandleFunc("/users/{id}/delete", func(w http.ResponseWriter, r *http.Request) {
+
+		id := mux.Vars(r)["id"]
+		idInt, err := strconv.ParseUint(id, 10, 64)
+		if err != nil {
+			fmt.Printf("Error parsing id: %v\n", err)
+			return
+		}
+
+		fmt.Printf("%+v /n", mux.Vars(r))
+
+		err = db.Delete(&User{}, idInt).Error
+		if err != nil {
+			fmt.Printf("Ошибка удаления")
 			return
 		}
 
@@ -158,18 +200,86 @@ func main() {
 			return
 		}
 
-		deleteid := r.FormValue("deleteid")
-		deleteid_int, err := strconv.Atoi(deleteid)
-
-		err = db.Delete(&User{}, deleteid_int).Error
-		if err != nil {
-			fmt.Printf("Ошибка удаления \n")
-			return
-		}
 		http.Redirect(w, r, "/deleteform", http.StatusFound)
 	})
 
-	err = http.ListenAndServe(":8080", mux)
+	router.HandleFunc("/users/{id}/edit", func(w http.ResponseWriter, r *http.Request) {
+		id := mux.Vars(r)["id"]
+		idInt, err := strconv.ParseUint(id, 10, 64)
+		if err != nil {
+			fmt.Printf("id не число")
+			return
+		}
+
+		err = r.ParseForm()
+		if err != nil {
+			fmt.Printf("Ошибка формы: %v\n", err)
+			return
+		}
+
+		name := r.FormValue("name")
+		surname := r.FormValue("surname")
+		password := r.FormValue("password")
+		email := r.FormValue("email")
+
+		fmt.Printf("name: %v id: %v \n", name, idInt)
+
+		var chelovek User
+
+		err = db.Find(&chelovek, idInt).Error
+		if err != nil {
+			fmt.Printf("Говно вопрос: %v\n", err)
+			return
+		}
+
+		fmt.Printf("chelovek: %+v", chelovek.Name)
+
+		chelovek.Name = name
+		chelovek.Surname = surname
+		chelovek.Password = password
+		chelovek.Email = email
+
+		fmt.Printf("chelovek: %+v", chelovek.Surname)
+
+		err = db.Save(chelovek).Error
+		if err != nil {
+			fmt.Printf("ошибка человека")
+		}
+
+		http.Redirect(w, r, "/edit", http.StatusFound)
+	})
+
+	router.HandleFunc("/profile/{id}", func(w http.ResponseWriter, r *http.Request) {
+
+		tmpl, err := template.ParseFiles("static/profile.tmpl")
+		if err != nil {
+			fmt.Printf("Error parsing template: %v\n", err)
+			return
+		}
+
+		id := mux.Vars(r)["id"]
+		idInt, err := strconv.ParseUint(id, 10, 64)
+		if err != nil {
+			fmt.Printf("id не число  2")
+			return
+		}
+
+		var chelovek User
+
+		err = db.Find(&chelovek, idInt).Error
+		if err != nil {
+			fmt.Printf("Ошибка не найден человек")
+		}
+
+		err = tmpl.Execute(w, chelovek)
+		if err != nil {
+			fmt.Printf("Error executing template: %v\n", err)
+			return
+		}
+
+	})
+
+	err = http.ListenAndServe(":8080", router)
 	if err != nil {
 		log.Fatal(err)
 	}
